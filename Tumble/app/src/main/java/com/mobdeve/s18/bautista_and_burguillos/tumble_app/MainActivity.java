6cd0ff9c399581cd472125bbe0126181ceedeb84
programmernammer <programmernammer@gmail.com>
2021-08-14 10:12:24 +0800

    Tumble main menu layout added

---
+++ Tumble/app/src/main/java/com/mobdeve/s18/bautista_and_burguillos/tumble_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mobdeve.s18.bautista_and_burguillos.tumble_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
