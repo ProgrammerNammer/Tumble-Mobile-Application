3fc04ad598ee02361b7cd7ea86fa98171e033b66
programmernammer <programmernammer@gmail.com>
2021-08-15 14:37:48 +0800

    Gameboard initial sketch added

---
+++ Tumble/app/src/main/java/com/mobdeve/s18/bautista_and_burguillos/tumble_app/GameActivity.java
@@ -0,0 +1,14 @@
+package com.mobdeve.s18.bautista_and_burguillos.tumble_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
